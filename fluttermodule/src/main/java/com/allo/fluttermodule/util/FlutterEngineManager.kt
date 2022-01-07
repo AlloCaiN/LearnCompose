8413a049602fc7a772960ba2222649042dd826c2
AlloCaiN <allocai0420@gmail.com>
2022-01-07 20:47:26 +0800

    实现compose 和 flutter互相调用

--- fluttermodule/src/main/java/com/allo/fluttermodule/util/FlutterEngineManager.kt
+++ fluttermodule/src/main/java/com/allo/fluttermodule/util/FlutterEngineManager.kt
@@ -1,6 +1,7 @@
 package com.allo.fluttermodule.util
 
 import android.content.Context
+import com.allo.fluttermodule.event.HybridRouteAndMessageHandleCenter
 import com.allo.fluttermodule.manager.AlloFlutterApplication
 import io.flutter.FlutterInjector
 import io.flutter.embedding.android.FlutterActivity
@@ -9,6 +10,8 @@ import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
 
 object FlutterEngineManager {
+    const val ENGINE_ID = "render"
+    const val RENDER_ENTRY = "render"
     // 如果有就复用，如果没有就重建,这边传入的参数主要形式有engineId 和 entryPoint两个
     fun flutterEngine(context : Context, engineId : String, entryPoint : String ): FlutterEngine? {
         // 通过系统的cache去获取缓存
@@ -24,7 +27,14 @@ object FlutterEngineManager {
     }
 
     fun routeToFlutter(context: Context) {
-        flutterEngine(context,"render","render")
+//        flutterEngine(context,"render","render")
+        val messageCenter = HybridRouteAndMessageHandleCenter(context)
         context.startActivity(FlutterActivity.withCachedEngine("render").build(context))
     }
+
+
+    fun destroyEngine() {
+        FlutterEngineCache.getInstance().get(ENGINE_ID)?.destroy()
+    }
+
 }
